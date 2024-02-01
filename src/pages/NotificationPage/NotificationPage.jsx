ebaf7b0a87d5b308d5d954386447cc00704c7cb6
NurdinDjumabekov <dui.nurdin@gmail.com>
2024-02-01 12:08:22 +0600

    почти всё закончил

--- src/pages/NotificationPage/NotificationPage.jsx
+++ src/pages/NotificationPage/NotificationPage.jsx
@@ -17,32 +17,72 @@ const NotificationPage = () => {
   React.useEffect(() => {
     dispatch(toTakeNotification({ tokenA }));
     const newData = listNotifs?.map((i) => i?.codeid);
-    setTimeout(() => {
+    return () => {
       dispatch(notificationRead({ tokenA, notifs: newData }));
-    }, 2000);
+    };
   }, []);
 
-  console.log(listNotifs, "listNotifs");
+  const sortNotif = (arr) => {
+    const lookList = arr?.filter((i) => (+i?.is_read === 1 ? i : ""));
+    const lookNoList = arr?.filter((i) => (+i?.is_read === 0 ? i : ""));
+    console.log(lookList, lookNoList);
+    return {
+      lookList,
+      lookNoList,
+    };
+  };
 
   return (
     <div className="mainTables">
       <div className="notification">
         <div className="notification__titles">
-          <h3>Дата</h3>
+          <h3>Дата и время</h3>
           <h3>Номер иска</h3>
           <h3>Статус</h3>
         </div>
-        {listNotifs?.map((notif) => (
+        {sortNotif(listNotifs)?.lookNoList?.length === 0 ? (
+          <></>
+        ) : (
+          <>
+            <h4>Новые уведомления</h4>
+            {sortNotif(listNotifs)?.lookNoList?.map((notif) => (
+              <div
+                className="notification__every"
+                key={notif?.codeid}
+                style={
+                  notif?.comment?.includes("Отклонён")
+                    ? { color: "red", background: "rgba(242 ,195 ,195, 0.43)" }
+                    : {
+                        color: "green",
+                        background: "rgba(159, 217, 151, 0.24)",
+                      }
+                }
+              >
+                <p>
+                  {notif?.date}
+                  <span style={{ marginLeft: "10px" }}>{notif?.time}</span>
+                </p>
+                <p>{notif?.isk_number}</p>
+                <p>{notif?.comment}</p>
+              </div>
+            ))}
+            <div className="line"></div>
+          </>
+        )}
+        {sortNotif(listNotifs)?.lookList?.map((notif) => (
           <div
             className="notification__every"
             key={notif?.codeid}
             style={
-              notif?.comment?.includes("Принят")
-                ? { color: "green" }
-                : { color: "red" }
+              notif?.comment?.includes("Отклонён")
+                ? { color: "red" }
+                : { color: "green" }
             }
           >
-            <p>{notif?.date}</p>
+            <p>
+              {notif?.date}
+              <span style={{ marginLeft: "10px" }}>{notif?.time}</span>
+            </p>
             <p>{notif?.isk_number}</p>
             <p>{notif?.comment}</p>
           </div>
